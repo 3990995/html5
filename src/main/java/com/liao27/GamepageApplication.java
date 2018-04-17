264c0b4283285e5a3a4605137fa6d08468888dcb
lwx459797 <liaohanqing@huawei.com>
2018-04-17 11:20:07 +0800

    c

---
+++ src/main/java/com/liao27/GamepageApplication.java
@@ -0,0 +1,12 @@
+package com.liao27;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamepageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamepageApplication.class, args);
+	}
+}
